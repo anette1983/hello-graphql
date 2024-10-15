09e4ae639a95b13d0907e08d0161ea24bc1b4477
Hanna K <anna_kon@bigmir.net>
2024-10-15 23:20:32 +0300

    add in-memory db,  createProduct and getProduct resolvers

--- graphql-tr/data/schema.js
+++ graphql-tr/data/schema.js
@@ -15,8 +15,26 @@ const schema = buildSchema(`
     }
 
     type Query {
-        product: Product
+        getProduct(id: ID): Product
     }
+
+     input StoreInput {
+        store: String
+    }
+
+    input ProductInput {
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Boolean
+        stores: [StoreInput]!
+    }
+
+    type Mutation {
+        createProduct(input: ProductInput): Product
+    }
+
     `);
 
 export default schema;
