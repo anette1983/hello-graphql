5117b22ca2a9b47b794b627c130dc94254e65c60
Hanna K <anna_kon@bigmir.net>
2024-10-16 20:01:09 +0300

    add local mongoDB connection

--- graphql-tr/data/resolvers.js
+++ graphql-tr/data/resolvers.js
@@ -1,3 +1,5 @@
+import { Widgets } from './dbConnectors';
+
 class Product {
 	constructor(id, { name, description, price, soldout, stores }) {
 		this.id = id;
@@ -12,18 +14,26 @@ class Product {
 const productDatabase = {};
 
 const resolvers = {
-	getProduct: ({ id }) => {
-		return new Product(id, productDatabase[id]);
+	// getProduct: ({ id }) => {
+	// 	return new Product(id, productDatabase[id]);
+	// },
+	getProduct: async ({ id }) => {
+		try {
+			const product = await Widgets.findById(id);
+			return product;
+		} catch (error) {
+			throw new Error(error);
+		}
 	},
-	getAllProducts: () => {
-		return Object.keys(productDatabase).map(
-			(id) => new Product(id, productDatabase[id])
-		);
-	},
+	// getAllProducts: () => {
+	// 	return Object.keys(productDatabase).map(
+	// 		(id) => new Product(id, productDatabase[id])
+	// 	);
+	// },
 	createProduct: ({ input }) => {
-		let id = require('crypto').randomBytes(10).toString('hex');
-		productDatabase[id] = input;
-		return new Product(id, input);
+		// let id = require('crypto').randomBytes(10).toString('hex');
+		// productDatabase[id] = input;
+		// return new Product(id, input);
 	},
 };
 
